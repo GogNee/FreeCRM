6f1a93312f83bbfb3f0e323ee5bfca7b2c03aebc
gognee <neeti.gogo@gmail.com>
2019-04-02 22:47:26 +0100

    Added new test page :SearchPageTest

---
+++ src/test/java/com/crm/qa/testcases/SearchPageTest.java
@@ -0,0 +1,13 @@
+package com.crm.qa.testcases;
+
+
+import java.io.File;
+
+import com.crm.qa.base.TestBaseClass;
+public class SearchPageTest extends TestBaseClass {
+	
+	SearchPageTest(){
+	System.out.println("I am inside SetUp:");
+	}
+
+}
